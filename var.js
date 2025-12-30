ec6a889e7d233b993b1e3ab497cab6cc0734987f
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2025-12-30 14:45:37 +0000

    Completed learning variable

---
+++ var.js
@@ -0,0 +1,18 @@
+const accId= 1445
+
+let accountEmail = "st@gmail.com"
+var accountPass = "12345"
+let accountState;
+accCity = "Jaipur" //not recomended
+// accId = 433 //not allowed
+accountEmail="sat@gmail.com" //allowed
+
+accountPass = "3248124809"
+
+accCity = "Pune"
+console.log(accountPass);
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accId,accountEmail,accountPass,accCity,accountState])
