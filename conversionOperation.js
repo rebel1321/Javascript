d96b28cc92d3b858c0c841f3b524dcc6cf15e308
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2025-12-31 16:27:45 +0000

    learn conversion operatioin

---
+++ conversionOperation.js
@@ -0,0 +1,26 @@
+let someNumber =33;
+let stringNumber =String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+let value =3
+let negValue = -value
+console.log(negValue)
+console.log(2**3)
+
+let str1 ="hello"
+let  str2 =" hitesh"
+
+let str3 = str1+ str2
+
+console.log(str3)
+
+// console.log(1+"3") 
+
+// console.log("1"+"3")
+// console.log("1"+1+2)
+// console.log(1+2+"3")
+
+console.log(+true)
+console.log(+"")
