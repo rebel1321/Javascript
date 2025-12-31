d631463ee7fec71c930e525829c628db9f7e2d96
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2025-12-31 04:00:06 +0000

    revised datatypes

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+// alert() we are using nodejs not browser 
+
+
+console.log(3
+    +3) //code readability must be high
+
+let name = "hitesh"
+let age = 19
+let isLoggedin = false
+let state;
+
+//number =>> 2 to power of 53
+// bigInt 
+// string => ""
+//boolean => true /false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+// object 
+//null is object type
+//undefined is itself a type
+
+console.log(typeof null)
+consdole.log(typeof undefined)
