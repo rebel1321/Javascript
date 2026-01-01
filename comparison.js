445cce7a00a9d03970664d7fd36172c68ca7ff72
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2026-01-01 03:12:47 +0000

    revised comparison

---
+++ comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+
+// console.log("2">1);
+
+// // data type must be same in case of comparison
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null>=0);
+
+console.log(undefined>0);
+console.log(null == undefined);
+console.log(undefined>=0);
+console.log(undefined==0);
+
+console.log(typeof undefined)
+console.log(typeof null)
