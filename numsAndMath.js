62c499aec56b89a707986e1d558394a78d3fae05
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2026-01-04 05:32:11 +0000

    Math and num

---
+++ numsAndMath.js
@@ -0,0 +1,29 @@
+const score =400
+const balance =new Number(100)
+// console.log(balance.toFixed(2))
+
+// console.log(typeof balance.toString())
+
+const otherNumber = 23.87867
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toFixed(3))
+
+
+const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//++++++++++++++++Maths+++++++++++++++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.pow(2,4))
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3)); // print 5  always took above value 
+// console.log(Math.floor(4.9)); //print 4 always took lower value
+
+console.log(Math.floor(Math.random()*10)+1)
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()* (max-min+1))+min)
