6de21c7dfa1638f4f88173624c93cf02846c6429
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2026-01-10 22:45:53 +0000

    revised function

--- functions.js
+++ functions.js
@@ -30,4 +30,33 @@ function loginUser(userName="sam"){
     return `${userName} just logged in`
 }
 
-console.log(loginUser());
+// console.log(loginUser());
+
+function calCart(val1, val2,...num1){
+    return num1
+}
+
+console.log(calCart(200,40,87,98 ,400))
+
+const user = {
+    name:"gyguy",
+    price:908
+}
+
+function handleObj(anyObj){
+   console.log(`user is ${anyObj.name} and price is ${anyObj.price}`);
+    
+}
+
+handleObj(user)
+handleObj({
+    name:"trd",
+    price:899
+})
+const myarr = [ 332,332,44,22]
+function returnSecVal(getArray){
+    return getArray[1];
+}
+console.log(returnSecVal(myarr));
+console.log(returnSecVal([3,4,5,6]));
+
