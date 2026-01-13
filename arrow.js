17b2f254d22f411d05ad1e01ad599e289c16467e
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2026-01-13 00:42:52 +0000

    revised arrow function

---
+++ arrow.js
@@ -0,0 +1,48 @@
+
+const user = {
+    username:"hitesh",
+    price:999,
+    welcomeMsg: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+}
+// user.welcomeMsg()
+// user.username="satyam"
+// user.welcomeMsg()
+
+// console.log(this);
+
+
+function chai (){
+    let username = "hitesh"
+    console.log(this.username); //undefined as this only work with object
+    console.log(this);
+    
+}
+chai()
+
+// const chai = function(){
+//      let username = "hitesh"
+//     console.log(this.username);// same output as before
+// }
+
+const chai2 = ()=>{ // arrow function
+     let username = "hitesh"
+    console.log(this.username);// same output as before
+    console.log(this);
+    
+}
+chai2()
+
+// const add = (num1,num2)=>{return num1+num2}
+// const add = (num1,num2)=> num1+num2
+// const add = (num1,num2)=>( num1+num2)
+const add = (num1,num2)=> ({user:"hitesh"})
+
+console.log(add(4,8));
+
+const myArr = [6,9,9,54]
+
+// myArr.forEach(()=>{})
