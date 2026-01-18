e5cdf82ca166c0e0446446e43d216e4e47cfcd07
Satyam Tripathi <satyam_22134503026@hnbgu.edu.in>
2026-01-18 01:43:12 +0000

    Revised while do while

---
+++ loops/while.js
@@ -0,0 +1,12 @@
+//while loop
+
+// while (2==2) {
+//     console.log("Rohit daddy double hundred");
+    
+// }
+let index = 14
+do {
+        console.log("Rohit daddy double hundred");
+        index++;
+
+} while (index<=14);
